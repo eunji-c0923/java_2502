a836b4b9e4b833bbc95213591b5cfdc1f0a0d5af
java2420 <didi09@naver.com>
2025-02-28 17:59:44 +0900

    java250228

--- java0225027_연산자/src/ch01/ex06/Logica10peratorExample.java
+++ java0225027_연산자/src/ch01/ex06/Logica10peratorExample.java
@@ -5,16 +5,16 @@ public class Logica10peratorExample {
 	public static void main(String[] args) {
 
 
-		int score = 85;
+		int score = 70;
 		char grade = (score > 90) ? 'A' : ( (score > 80) ? 'B' : 'C');
+		System.out.println(score + "합격" + grade + "불합격.");
+		
+		int score = 65;
+		char grade = (score > 70) ? 'A' : ( (score > 60) ? 'B' : 'C');
 		System.out.println(score + "점은 " + grade + "등급입니다.");
 		
-		int score = 60;
-		char grade = (score > 80) ? 'A' : ( (score > 50) ? 'B' : 'C');
-		System.out.println(score + "점은 " + grade + "등급입니다.");
 		
-		
-		
+	//숫자만 바꿨는데 오류가 뜸.....물어봐야지
 		
 		
 		
